950cb859b1f56dae64bb79132ac9b60db4842acb
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-05-06 19:32:48 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Boris R on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
